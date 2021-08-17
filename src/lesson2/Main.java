b5d0f12870a8aa0892b5f9b580de9b71a49917a0
AlekseiDomashkevich <adomashkevich@gmail.com>
2021-08-17 17:47:09 +0300

    feat: add task one

--- src/lesson2/Main.java
+++ src/lesson2/Main.java
@@ -1,7 +1,16 @@
 package lesson2;
 
 public class Main {
+    public static void taskOne() {
+        double dayNorm = 10;
+        for (int i = 0; i < 7; i++) {
+            dayNorm = dayNorm + dayNorm * 10 / 100;
+        }
+        System.out.printf("%.1f", dayNorm);
+    }
+
     public static void main(String[] args) {
+        taskOne();
 
     }
 

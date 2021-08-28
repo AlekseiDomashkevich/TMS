53dc14cd1be2bc3d66bf60c74a36e9eb50b9402c
AlekseiDomashkevich <adomashkevich@gmail.com>
2021-08-28 20:09:09 +0300

    homework-5

---
+++ src/lesson5/phonebook/storage/DatabaseStorage.java
@@ -0,0 +1,10 @@
+package lesson5.phonebook.storage;
+
+import lesson5.phonebook.entity.Person;
+
+public class DatabaseStorage implements Storage {
+    @Override
+    public void save(Person save) {
+        ;
+    }
+}
---
+++ src/lesson5/phonebook/storage/Storage.java
@@ -0,0 +1,7 @@
+package lesson5.phonebook.storage;
+
+import lesson5.phonebook.entity.Person;
+
+public interface Storage {
+    void save(Person save);
+}

d2cc3b6c4260db90cad691526ee9b2ee584fbfda
AlekseiDomashkevich <adomashkevich@gmail.com>
2021-09-01 23:53:47 +0300

    homework-7

--- src/lesson5/phonebook/Application.java
+++ src/lesson5/phonebook/Application.java
@@ -1,45 +1,10 @@
 package lesson5.phonebook;
 
-import lesson5.phonebook.dao.PhonebookDAO;
-import lesson5.phonebook.mapper.InputMapper;
-import lesson5.phonebook.storage.FileStorage;
-
-import java.util.Collections;
-
+// http://test.com/path/to/resource
+// http://test.com/application/save
 public class Application {
     public static void main(String[] args) {
-        var action = args[0];
-        //var dao = new PhonebookDAO(Arrays.asList(new FileStorage(), new DatabaseStorage()));
-        var dao = new PhonebookDAO(Collections.singletonList(new FileStorage()));
-        switch (action) {
-            case "update" -> {
-                dao.update(Integer.parseInt(args[1]), args[2]);
-            }
-            case "generate" -> {
-                var mapper = new InputMapper();
-                for (int i = 0; i < 15; i++) {
-                    dao.save(mapper.toEntity(args));
-                }
-            }
-            case "save" -> {
-                var mapper = new InputMapper();
-                dao.save(mapper.toEntity(args));
-            }
-            case "call" -> {
-                var person = dao.findByLastname(args[1]);
-                //person.getPhoneNumber();
-            }
-            case "delete" -> {
-                dao.delete(Integer.parseInt(args[1]));
-            }
-            case "find" -> {
-                var person = dao.find(Integer.parseInt(args[1]));
-                if (person != null) {
-                    System.out.println(person);
-                } else {
-                    System.err.println("Person not found");
-                }
-            }
-        }
+        var router = new Router(args);
+        router.dispatch();
     }
 }
